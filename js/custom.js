4ecdb7477bda7cb531a390ea15ffcbe7192e3d5d
Zulfat Ilyasov <mail@zulfat.net>
2014-10-09 14:44:21 +0400

    first nav click

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,7 @@
 'use strict';
 
 $(function() {
-
+    var body = $('html, body');
     $('.load-more').click(function() {
         $('.testimonials-section').addClass('open');
         $(this).hide();
@@ -15,4 +15,23 @@ $(function() {
     $('.overlay').click(function() {
         $('.close-icon').trigger('click');
     });
+    $('.w-nav-link').click(function() {
+        if ($(this).is('.first-nav')) {
+            body.animate({
+                scrollTop: 0
+            }, '1000');
+            window.location.hash = 'company';
+        }
+    });
+
+    $(window).scroll(function() {
+        if ($(window).scrollTop() < 224) {
+            if ($('.first-nav').is('.w--current') === false)
+                $('.first-nav').addClass('w--current');
+        } else {
+            $('.first-nav').removeClass('w--current');
+        }
+    });
+
+    $('.first-nav').click(function() {});
 });
