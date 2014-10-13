f7fe1a1c2f2fac91221e902843274f5250ec5af0
Зульфат Ильясов <zulfat@Zulfats-MacBook-Pro.local>
2014-10-14 00:12:32 +0400

    reported issues fixed. media queries added

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,21 @@
 'use strict';
 
-$(function() {
+$(function () {
     var body = $('html, body');
-    $('.load-more').click(function() {
+    $('.load-more').click(function () {
         $('.testimonials-section').addClass('open');
         $(this).hide();
     });
-    $('[data-ix="show-modal"]').click(function() {
+    $('[data-ix="show-modal"]').click(function () {
         $('.overlay').addClass('open');
     });
-    $('.close-icon').click(function() {
+    $('.close-icon').click(function () {
         $('.overlay').removeClass('open');
     });
-    $('.overlay').click(function() {
+    $('.overlay').click(function () {
         $('.close-icon').trigger('click');
     });
-    $('.w-nav-link').click(function() {
+    $('.w-nav-link').click(function () {
         if ($(this).is('.first-nav')) {
             body.animate({
                 scrollTop: 0
@@ -24,24 +24,13 @@ $(function() {
         }
     });
 
-    $(window).scroll(function() {
+    $(window).scroll(function () {
         if ($(window).scrollTop() < 224) {
             if ($('.first-nav').is('.w--current') === false)
                 $('.first-nav').addClass('w--current');
         } else {
             $('.first-nav').removeClass('w--current');
         }
-        // if ($(window).scrollTop() >= 1240) {
-        //     $('.products-section').addClass('fixed-products');
-        //     $('.process-section').addClass('mt-process');
-        // } else {
-        //     if ($('.products-section').is('.fixed-products')) {
-        //         $('.products-section').removeClass('fixed-products');
-        //     }
-        //     if ($('.process-section').is('.mt-process')) {
-        //         $('.process-section').removeClass('mt-process');
-        //     }
-        // }
 
         if ($(window).scrollTop() >= 720) {
             $('.slider-section').hide();
@@ -49,6 +38,4 @@ $(function() {
             $('.slider-section').show();
         }
     });
-
-    $('.first-nav').click(function() {});
 });
